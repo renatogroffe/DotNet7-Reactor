5c57124364b18e549d10ea16787bce00eb2a04a1
Renato Groffe <renato.groffe@yahoo.com.br>
2022-11-16 17:36:10 -0300

    Commit inicial
    Commit inicial

---
+++ CSharp11/ExemploRawStringLiterals/Program.cs
@@ -0,0 +1,25 @@
+﻿using ExemploRawStringLiterals;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Text.Json;
+
+Console.WriteLine("***** Testes com C# 11 | Raw String Literals *****");
+Console.WriteLine($"Versao do .NET em uso: {RuntimeInformation
+    .FrameworkDescription} - Ambiente: {Environment.MachineName}- Kernel: {Environment
+    .OSVersion.VersionString}");
+
+var pessoa1 = "{ \"Nome\": \"Renato Groffe\", \"Cidade\": \"Sao Paulo\", \"Idade\": 40 }";
+Teste.ExibirConteudo(pessoa1);
+
+var pessoa2 = """{ "Nome": "Joao Oliveira", "Cidade": "Rio de Janeiro", "Idade": 20 }""";
+Teste.ExibirConteudo(pessoa2);
+
+var pessoa3 =
+    """
+       {
+         "Nome": "Maria da Silva",
+         "Cidade": "Belo Horizonte",
+         "Idade": 32
+       }
+    """;
+Teste.ExibirConteudo(pessoa3);
